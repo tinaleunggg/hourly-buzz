949b15fb35ef6a43cb9888a78b446e5e61a4d913
tina.leunggg <tinaisworking@gmail.com>
2025-07-12 17:06:38 -0700

    edit CORS

--- backend/app.py
+++ backend/app.py
@@ -2,21 +2,24 @@ from mongo_connect import db
 from flask import *
 from bson.json_util import dumps
 from bson.objectid import ObjectId
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # allow all origin now, need to change later for security
+
 restaurants = db["restaurants"]
 
 @app.route("/api/restaurant_listing", methods=["GET"])
 def get_restaurant_listing():
     doc = restaurants.find()
     result = dumps(list(doc), indent= 4)
-    return result
+    return Response(result, mimetype="application/json")
     
 @app.route("/api/restaurant_detail/<id>", methods=["GET"])
 def get_restaurant_detail(id):
     doc = restaurants.find_one({"_id": ObjectId(id)})
     result = dumps(doc, indent=4)
-    return result
+    return Response(result, mimetype="application/json")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
